dd0781cc71456c8122073ab376357845865f8b7e
storres8 <stevetorres7562@gmail.com>
2019-07-13 21:08:50 -0400

    updated comments.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,6 +24,8 @@ export class HomeComponent implements OnInit, OnDestroy {
   // ngOnDestroy allows us to run a function upoun us leaving this specific component.
   ngOnDestroy() {
     // upon us leaving the home component we unsubscribe to the observable created above.
+    // once we come back to this component the observable starts again from the beggining, not
+    // from where it left.
     this.firstObsSubscription.unsubscribe();
   }
 }
