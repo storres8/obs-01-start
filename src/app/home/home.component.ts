76b54b812603851f539ebcce78ccace5200d05e2
storres8 <stevetorres7562@gmail.com>
2019-07-13 20:49:20 -0400

    created 1st observable and also made sure to manually unsubscribe.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { interval, Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  // we create this private property to store our subscription which we can then have access to
+  // thorughout the rest of the component.
+  // By storing it in a property we can then reference the subscription later in ngOnDestroy so that we may
+  // unsubscribe from the observable when we leave this compnent.
+  private firstObsSubscription: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.firstObsSubscription = interval(1000).subscribe(count => {
+      console.log(count);
+    });
   }
 
+  // ngOnDestroy allows us to run a function upoun us leaving this specific component.
+  ngOnDestroy() {
+    // upon us leaving the home component we unsubscribe to the observable created above.
+    this.firstObsSubscription.unsubscribe();
+  }
 }
